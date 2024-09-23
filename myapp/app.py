d027c3e8779841a58ef0d91461ae45072b4eb951
STEM Mavericks <stemavericks.team@outlook.com>
2024-09-23 15:11:21 +0000

    Auto Commit

--- myapp/app.py
+++ myapp/app.py
@@ -1,2 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_sqlalchemy 
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email
+from flask
