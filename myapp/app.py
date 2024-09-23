80297c045490e2004edca9eb24712fd3a927f7a7
STEM Mavericks <stemavericks.team@outlook.com>
2024-09-23 15:21:31 +0000

    Auto Commit

--- myapp/app.py
+++ myapp/app.py
@@ -3,4 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email
-from flask
+from flask_oauthlib.client import OAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+from flask_mail import Mail, Message
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# OAuth Config For GitHub
+app.config['GITHUB_CLIENT_ID'] = os.getenv('GITHUB_CLIENT_ID')
+app.config['GITHUB_CLIENT_SECRET'] = os.getenv('GITHUB_CLIENT_SECRET')
